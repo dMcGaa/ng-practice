f67d51aa3e32acd73d9575fc46d28db65944d7f1
dmcgaa <mcgaa.david@gmail.com>
2016-11-10 04:51:08 +0000

    using a factory for data

--- public/simpleApp.js
+++ public/simpleApp.js
@@ -1,9 +1,25 @@
 var demoApp = angular.module('simpleApp', []);
 
-demoApp.controller('SimpleController', function($scope){
-    $scope.people = [
+demoApp.factory('simpleFactory', function(){
+    var people = [
         {name: 'david', profession: 'developer'},
         {name: 'samson', profession: 'pilot'},
         {name: 'beck', profession: 'drummer'}
     ];
+    
+    var factory = {};
+    factory.getCustomers = function(){
+        return people;
+    }
+    return factory;
+});
+
+demoApp.controller('SimpleController', function($scope, simpleFactory){
+    $scope.people = [];
+    
+    init();
+    
+    function init(){
+        $scope.people = simpleFactory.getCustomers();     
+    }
 });
